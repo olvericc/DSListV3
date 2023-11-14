30ed789ad729f28467b89573d98a8be2e2b76ee8
olvericc <olv.ericlopes@gmail.com>
2023-11-13 21:26:58 -0300

    project: creating the spring boot service

---
+++ src/main/java/com/olvericc/dslistv3/Dslistv3Application.java
@@ -0,0 +1,13 @@
+package com.olvericc.dslistv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslistv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslistv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/olvericc/dslistv3/Dslistv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olvericc.dslistv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslistv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
